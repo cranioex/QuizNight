b4eb9b18ee50b5cd365e1e8722e720f8a6729540
Eudimar Souza <cranioex@me.com>
2020-04-08 12:47:56 -0300

    Initial Commit

---
+++ QuizNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizNight
+//
+//  Created by EdySouza on 08/04/20.
+//  Copyright © 2020 EdySouza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
